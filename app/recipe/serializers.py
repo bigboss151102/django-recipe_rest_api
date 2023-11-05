7b9933c5569921f1280a6067ce61cf243242e9dc
hoangtrong <hoangcongtrong054@gmail.com>
2023-11-06 06:36:28 +0700

    implement update recipe tag feature API

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -26,6 +26,16 @@ class RecipeSerializer(serializers.ModelSerializer):
         fields = ['id', 'title', 'time_minutes', 'price', 'link', 'tags']
         read_only_fields = ['id']
 
+    def _get_or_create_tags(self, tags, recipe):
+        """Handle getting or creating tags as needed."""
+        auth_user = self.context['request'].user
+        for tag in tags:
+            tag_obj, created = Tag.objects.get_or_create(
+                user=auth_user,
+                **tag,
+            )
+            recipe.tags.add(tag_obj)
+
     def create(self, validated_data):
         """Create a recipe."""
         """
@@ -33,23 +43,37 @@ class RecipeSerializer(serializers.ModelSerializer):
         """
         tags = validated_data.pop('tags', [])
         recipe = Recipe.objects.create(**validated_data)
-        auth_user = self.context['request'].user
-        for tag in tags:
-            """
-            Dòng này thực hiện việc tìm kiếm hoặc tạo mới một đối tượng "Tag."
-            Nếu tag đã tồn tại với thông tin như đã cung cấp (dựa trên người dùng và thông tin tag),nó sẽ được tìm kiếm.
-            Nếu không tồn tại, nó sẽ được tạo mới.
-            """
-            tag_obj, created = Tag.objects.get_or_create(
-                user=auth_user,
-                **tag,
-            )
-            """
-            Thêm đối tượng "tag_obj" vào trường "tags" của đối tượng recipe.
-            """
-            recipe.tags.add(tag_obj)
+        # auth_user = self.context['request'].user
+        # for tag in tags:
+        #     """
+        #     Dòng này thực hiện việc tìm kiếm hoặc tạo mới một đối tượng "Tag."
+        #     Nếu tag đã tồn tại với thông tin như đã cung cấp (dựa trên người dùng và thông tin tag),nó sẽ được tìm kiếm.
+        #     Nếu không tồn tại, nó sẽ được tạo mới.
+        #     """
+        #     tag_obj, created = Tag.objects.get_or_create(
+        #         user=auth_user,
+        #         **tag,
+        #     )
+        #     """
+        #     Thêm đối tượng "tag_obj" vào trường "tags" của đối tượng recipe.
+        #     """
+        #     recipe.tags.add(tag_obj)
+        self._get_or_create_tags(tags, recipe)
 
         return recipe
+
+    def update(self, instance, validated_data):
+        """Update recipe."""
+        tags = validated_data.pop('tags', None)
+        if tags is not None:
+            instance.tags.clear()
+            self._get_or_create_tags(tags, instance)
+
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+
+        instance.save()
+        return instance
 
 
 class RecipeDetailSerializer(RecipeSerializer):
